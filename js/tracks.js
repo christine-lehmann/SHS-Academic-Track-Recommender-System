3ac43e0a887aa58b1137188284d981c570d4d9c6
Crizien Navarro <66468156+ziennn@users.noreply.github.com>
2022-12-02 12:02:33 +0800

    Add files via upload

---
+++ js/tracks.js
@@ -0,0 +1,42 @@
+const serviceItems = document.querySelector(".service-items");
+const serviceItems2 = document.querySelector(".service-items2");
+const popup = document.querySelector(".popup-box")
+const popupCloseBtn = popup.querySelector(".popup-close-butn");
+const popupCloseIcon = popup.querySelector(".popup-close-icon");
+
+   serviceItems.addEventListener("click",function(event){
+      if(event.target.tagName.toLowerCase() == "button"){
+         const item =event.target.parentElement;
+         const h3 = item.querySelector("h3").innerHTML;
+         const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+         popup.querySelector("h3").innerHTML = h3;
+         popup.querySelector(".popup-body").innerHTML = readMoreCont;
+         popupBox();
+      }
+   })
+
+   serviceItems2.addEventListener("click",function(event){
+      if(event.target.tagName.toLowerCase() == "button"){
+         const item =event.target.parentElement;
+         const h3 = item.querySelector("h3").innerHTML;
+         const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+         popup.querySelector("h3").innerHTML = h3;
+         popup.querySelector(".popup-body").innerHTML = readMoreCont;
+         popupBox();
+      }
+   })
+
+popupCloseBtn.addEventListener("click", popupBox);
+popupCloseIcon.addEventListener("click", popupBox);
+
+popup.addEventListener("click", function(event){
+     if(event.target == popup){
+        popupBox();
+     }
+})
+
+function popupBox(){
+    popup.classList.toggle("open");
+}
+
+

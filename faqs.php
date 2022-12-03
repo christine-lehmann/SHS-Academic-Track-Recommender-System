a927af482530ba673906f8b408e4cbbb980de8e7
Christine Cion <cion.c.bscs@gmail.com>
2022-12-03 21:20:12 +0800

    update faqs.php

--- faqs.php
+++ faqs.php
@@ -29,7 +29,7 @@
         <a href="home.php">HOME</a>
         <a href="about.php">ABOUT</a>
         <a href="tracks.php">STRANDS</a>
-        <a href="test_form.php">ASSESSMENT</a>
+        <a href="guide.php">ASSESSMENT</a>
         <a href="faqs.php">FAQS</a>
         
     </nav>

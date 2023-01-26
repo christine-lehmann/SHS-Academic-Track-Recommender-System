cc36c7fb1917753f84a9e9d5163baa8335652aee
Christine Cion <cion.c.bscs@gmail.com>
2023-01-26 11:45:54 +0800

    changed title page

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Academic Track Recommender</title>
+    <title>SHS Academic Track Recommender System</title>
     <link rel="shortcut icon" type="image/x-icon" href="images/SaTRSlogo.png" />
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 

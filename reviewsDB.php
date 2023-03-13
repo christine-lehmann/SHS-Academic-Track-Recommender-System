a6199630eca181facd864fca4a53fd22b80cd666
Christine Cion <cion.c.bscs@gmail.com>
2023-03-13 11:45:03 +0800

    rename database

--- reviewsDB.php
+++ reviewsDB.php
@@ -3,7 +3,7 @@
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'riasec_test';
+$DATABASE_NAME = 'satrs_db';
 try {
     $pdo = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
 } catch (PDOException $exception) {
